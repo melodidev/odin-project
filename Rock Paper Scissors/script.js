c990e8c0e67c963a574a048ff833a5042ce5e723
Ezgi Okka <ezgiokka@gmail.com>
2022-12-17 16:08:18 +0300

    Update rock paper scissors

--- Rock Paper Scissors/script.js
+++ Rock Paper Scissors/script.js
@@ -1,60 +1,73 @@
-let array = ["rock", "paper", "scissors"];
 let playerScore = 0;
 let computerScore = 0;
+let round = 0;
 
+let output = document.querySelector(".output");
+
+document.querySelectorAll("button").forEach(button => button.addEventListener("click", (event) => {
+  let playerSelection = button.dataset.key;
+  playRound(playerSelection, getComputerChoice());
+  round++;
+
+  if (round > 4) {
+    console.log(announceWinner(playerScore, computerScore));
+    return;
+  }
+}));
+
+let array = ["rock", "paper", "scissors"];
 let getComputerChoice = () => array[Math.floor(Math.random() * 3)];
-
-let playerSelection = () => {
-	let playerMove = prompt("What is your move?").toLowerCase();
-	return (array.includes(playerMove) ? playerMove : false);
-}
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection == computerSelection) {
-  	return "deuce";
+    output.textContent = "Tie.";
+    return;
   } else if (playerSelection == "rock") {
-  	if (computerSelection == "scissors") {
-  		playerScore++;
-  		return `Player's ${playerSelection} beats computer's ${computerSelection}!`;
-  	} else {
-  		computerScore++;
-  		return `Computer's ${computerSelection} beats player's ${playerSelection}.`;
-  	}
+    if (computerSelection == "scissors") {
+      playerScore++;
+      output.textContent = `Player's ${playerSelection} beats computer's ${computerSelection}!`;
+      return;
+    } else {
+      computerScore++;
+      output.textContent = `Computer's ${computerSelection} beats player's ${playerSelection}.`;
+      return;
+    }
   } else if (playerSelection == "paper") {
-  	if (computerSelection == "rock") {
-  		playerScore++;
-  		return `Player's ${playerSelection} beats computer's ${computerSelection}!`;
-  	} else {
-  		computerScore++;
-  		return `Computer's ${computerSelection} beats player's ${playerSelection}.`;
-  	}
+    if (computerSelection == "rock") {
+      playerScore++;
+      output.textContent = `Player's ${playerSelection} beats computer's ${computerSelection}!`;
+      return;
+    } else {
+      computerScore++;
+      output.textContent = `Computer's ${computerSelection} beats player's ${playerSelection}.`;
+      return;
+    }
   } else if (playerSelection == "scissors") {
-  	if (computerSelection == "paper") {
-  		playerScore++;
-  		return `Player's ${playerSelection} beats computer's ${computerSelection}!`;
-  	} else {
-  		computerScore++;
-  		return `Computer's ${computerSelection} beats player's ${playerSelection}.`;
-  	}
+    if (computerSelection == "paper") {
+      playerScore++;
+      output.textContent = `Player's ${playerSelection} beats computer's ${computerSelection}!`;
+      return;
+    } else {
+      computerScore++;
+      output.textContent = `Computer's ${computerSelection} beats player's ${playerSelection}.`;
+      return;
+    }
   }
 }
 
 function announceWinner (playerScore, computerScore) {
-	if (playerScore > computerScore) {
- 	return "You win!";
+  let result = document.querySelector(".button-container");
+  result.innerHTML = ""; 
+
+
+  if (playerScore > computerScore) {
+    output.textContent = "You win!";
+    return;
  } else if (playerScore < computerScore) {
- 	return "Robots beats us 〣( ºΔº )〣";
+  output.textContent = "Robots beats us 〣( ºΔº )〣";
+  return;
  } else {
- 	return "deuceee";
+  output.textContent = "Tie.";
+  return;
  }
 }
-
-function game() {
-	for (let i = 0; i < 5; i++) {
-		console.log(`${i + 1}. round:`);
-  	console.log(playRound(playerSelection(), getComputerChoice()));
- 	}
- 	console.log(announceWinner(playerScore, computerScore));
-}
-
-game();
