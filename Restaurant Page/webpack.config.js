a9c9bd0b0c39e9f8e80597375c9183f860378a7e
Ezgi Okka <ezgiokka@gmail.com>
2022-12-26 14:29:03 +0300

    Add index.html

--- Restaurant Page/webpack.config.js
+++ Restaurant Page/webpack.config.js
@@ -1,12 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-module: {
+  module: {
     rules: [
       {
         test: /\.css$/i,
